a2e7320cf09ddb17c29dfcdcf8ae2ba2b4852d60
AtsushiNi <atsushi9731@gmail.com>
2023-04-26 21:02:37 +0900

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [events, setEvents] = useState([])
+  useEffect(() => {
+    const searchEvents = async() => {
+      let response =  await new Promise((resolve, reject) => {
+        fetch("localhost:5000/search")
+          .then(res => res.json())
+          .then(data => resolve(data))
+      })
+      setEvents(searchEvents)
+    }
+    searchEvents()
+  })
+  fetch("http://localhost:5000/search")
+
   return (
     <div className="App">
       <header className="App-header">
