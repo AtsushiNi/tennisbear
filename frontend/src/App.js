c09b768b78e0eb05aea66860c0d2573ad9658bc4
AtsushiNi <atsushi9731@gmail.com>
2023-04-26 22:22:38 +0900

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,80 @@
 import { useEffect, useState } from 'react'
-import logo from './logo.svg';
 import './App.css';
+
+import { Row, Col, Button, Table, Avatar } from 'antd'
 
 function App() {
   const [events, setEvents] = useState([])
   useEffect(() => {
     const searchEvents = async() => {
-      let response =  await new Promise((resolve, reject) => {
-        fetch("localhost:5000/search")
-          .then(res => res.json())
-          .then(data => resolve(data))
-      })
-      setEvents(searchEvents)
+      let response =  await fetch("http://localhost:5000/search")
+      const newEvents = await response.json()
+      setEvents(newEvents)
     }
     searchEvents()
-  })
-  fetch("http://localhost:5000/search")
+  },[])
+
+  const columns = [
+    {
+      title: "主催者",
+      dataIndex: "organizer",
+      key: "organizer",
+      render: (text, { avatarSrc }) => {
+        if(text.length <= 10) return <><td><Avatar src={avatarSrc} /></td><td style={{fontSize: "5px"}}>{text}</td></>
+        return <><td><Avatar src={avatarSrc} /></td><td style={{fontSize: "5px"}}>{text.substr(0,8) + ".."}</td></>
+      }
+    },
+    {
+      title: "時刻",
+      dataIndex: "datetime",
+      key: "organizer",
+      render: text => <><p style={{fontSize: "5px"}}>{text.split(' ')[0]}</p><p style={{fontSize: "5px"}}>{text.split(' ')[1]}</p></>
+    },
+    {
+      title: "タイトル",
+      dataIndex: "title",
+      key: "title",
+      render: text => {
+        if(text.length <= 30) return <td style={{fontSize: "10px"}}>{text}</td>
+        return <td style={{fontSize: "10px"}}>{text.substr(0,30) + ".."}</td>
+      }
+    },
+    {
+      title: "レベル",
+      dataIndex: "level",
+      key: "level"
+    }
+  ]
+
+  const data = events.map((event, index) => ({
+    key: index,
+    avatarSrc: event.imageUrl,
+    organizer: event.organizer.name,
+    datetime: event.datetimeForDisplay,
+    title: event.eventTitle,
+    level: "Lv." + event.minLevel.id + "~" + event.maxLevel.id
+  }))
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <div style={{margin: "50px"}}>
+          <Row>
+            <Col>
+              <Button type="primary">東京都</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button type="primary">東京都</Button>
+            </Col>
+          </Row>
+
+          <Table dataSource={data} columns={columns} />
+        </div>
+      </body>
     </div>
   );
 }
